5df578f7575a57257f371b9c5cfb6b1c8857182b
Samuel Chan <chankaihei@gmail.com>
2025-04-14 03:48:43 +0800

    add button for deleting comment

--- controllers/comment.controller.php
+++ controllers/comment.controller.php
@@ -42,29 +42,20 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['add_comment'])) {
 }
 
 if (isset($_GET['action']) && $_GET['action'] === 'delete' && isset($_GET['id'])) {
-    $post_id = (int)$_GET['id'];;
-    $username = $_SESSION['username'];
+    $comment_id = (int)$_GET['id'];
 
-    $stmt = $conn->prepare("SELECT username FROM post WHERE id = ?");
-    $stmt->bind_param("i", $post_id);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    $post = $result->fetch_assoc();
-    $stmt->close();
-
-    if ($post) {
-            $stmt = $conn->prepare("DELETE FROM comment WHERE id = ?");
-            $stmt->bind_param("i", $post_id);
-            if ($stmt->execute()) {
-                $stmt->close();
-                echo "<script>alert('comment deleted successfully!'); window.location.href = '../views/dashboard.view.php';</script>";
-                exit();
-            } else {
-                $stmt->close();
-                echo "<script>alert('Failed to delete comment. Please try again.'); window.location.href = '../views/dashboard.view.php';</script>";
-                exit();
-            }
-        } 
+    $stmt = $conn->prepare("DELETE FROM comment WHERE id = ?");
+    $stmt->bind_param("i", $comment_id);
+    
+    if ($stmt->execute()) {
+        $stmt->close();
+        echo "<script>alert('Comment deleted successfully!'); window.location.href = '../views/dashboard.view.php';</script>";
+        exit();
+    } else {
+        $stmt->close();
+        echo "<script>alert('Failed to delete comment. Please try again.'); window.location.href = '../views/dashboard.view.php';</script>";
+        exit();
     }
+}
 ?>
 
