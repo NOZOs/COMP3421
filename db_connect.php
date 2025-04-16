faa01ca955f48966999db914150a8f8353795136
Your Name <you@example.com>
2025-04-16 22:10:44 +0800

    host

--- db_connect.php
+++ db_connect.php
@@ -12,9 +12,3 @@ $conn = new mysqli($servername, $username, $password, $dbname, $port);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
-echo "Connected successfully";
-
-// Close the connection
-$conn->close();
-?>
