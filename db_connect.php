ffe383ffb1532e8e7636ac1bca38e6a1358bd47d
Your Name <you@example.com>
2025-04-16 22:07:02 +0800

    test host

--- db_connect.php
+++ db_connect.php
@@ -1,12 +1,20 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "blog_db";
+$servername = "ehc1u4pmphj917qf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$username = "d9wcuyxwit45t4sh";
+$password = "az9ia1foe8wzpcd3";
+$dbname = "i9zcmu88t2blefy2";
+$port = 3306; // Default MySQL port
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+echo "Connected successfully";
+
+// Close the connection
+$conn->close();
 ?>
