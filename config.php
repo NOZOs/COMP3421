6b662c968d19d5af6a33362a149edaf68e3d48f7
NOZOs_eduuse <112964915+NOZOs@users.noreply.github.com>
2025-04-17 22:19:26 +0800

    Update config.php
    correct one

--- config.php
+++ config.php
@@ -1,14 +1,12 @@
 <?php
-$servername = "ehc1u4pmphj917qf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-$username = "d9wcuyxwit45t4sh";
-$password = "az9ia1foe8wzpcd3";
-$dbname = "i9zcmu88t2blefy2";
-$port = 3306;
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "blog_db";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname, $port);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+?>
